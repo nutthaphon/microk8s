423b4cfeb98ca39493dd9bae9a2f90f817b4d9a5
nutt-office <nutthaphon@v-smart.com>
2019-08-28 21:13:00 +0700

    ok

--- dashboard_access.sh
+++ dashboard_access.sh
@@ -1,5 +1,5 @@
-
 token=$(microk8s.kubectl -n kube-system get secret | grep default-token | cut -d " " -f1)
-microk8s.kubectl -n kube-system describe secret $token
+microk8s.kubectl -n kube-system describe secret $token | grep -e "^token" | cut -d: -f 2 | xargs | xclip -i
+xclip -o
 
-microk8s.kubectl get all --all-namespaces | grep "service/kubernetes-dashboard" | awk '{ print $4 }' | xargs -I{} chromium-browser https://{}
+microk8s.kubectl get all --all-namespaces | grep "service/kubernetes-dashboard" | awk '{ print $4 }' | xargs -I{} chromium-browser --new-window https://{} &
